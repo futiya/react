15de2cc1bac26ca9b9d66abbcc59f8c136159db1
jieqli <jieqli@cn.ibm.com>
2020-12-10 01:19:05 +0800

    lesson 6 My Hacker News

---
+++ src/lesson6/test/Fetch.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useFetch } from "./test/hooks"
+export function Fetch(
+    {
+        url,
+        renderSuccess,
+        loadingFallBack = <p>loading...</p>,
+        renderError = (error) => <pre>{JSON.stringify(error, null, 2)}</pre>,}
+){
+    console.log(url)
+  const { loading, data, error } = useFetch(url);
+  console.log("get data:", data)
+  if (loading) return loadingFallBack;
+  if (error) return renderError(error);
+  if (data) return renderSuccess({ data });
+}
---
+++ src/lesson6/test/RepoMenu.js
@@ -0,0 +1,14 @@
+
+import React, { useEffect } from "react";
+import { useIterator } from "./test/hooks/index"
+
+export default function RepoMenu({ repos, onSelect = (f) => f }){
+    const [{ name }, previous, next] = useIterator(repos)
+    return (
+        <div style={{ display: "flex" }}>
+          <button onClick={previous}>&lt;</button>
+          <p>{name}</p>
+          <button onClick={next}>&gt;</button>
+        </div>
+      );
+}
